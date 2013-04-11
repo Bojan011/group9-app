8b7db56bc86b36dfeb4caeb1d2e0aed38e1f4998
Bojan Djordjevic <bdjordje@ryerson.ca>
2013-04-11 11:53:41 -0400

    Display users from database

--- src/com/project/group9/ViewPlayers.java
+++ src/com/project/group9/ViewPlayers.java
@@ -1,26 +1,48 @@
 package com.project.group9;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.ListActivity;
-import android.content.Intent;
+import android.content.Context;
+/*import android.content.Intent;
+import android.content.SharedPreferences;*/
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.util.Log;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.savagelook.android.UrlJsonAsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class ViewPlayers extends ListActivity {
 	
-	static final String[] players = 
-            new String[] { "Andrew", "Dave", "Jamie", "Nick"};
-
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	
+		PlayerTask playerTask = new PlayerTask(ViewPlayers.this);
+        playerTask.setMessageLoading("Fetching Players...");
+        playerTask.execute();
 		
-		setListAdapter(new PlayerArrayAdapter(this, players));
 	
 	}
 
@@ -40,5 +62,86 @@ public class ViewPlayers extends ListActivity {
 		getMenuInflater().inflate(R.menu.view_players, menu);
 		return true;
 	}
+	
+	
+	private class PlayerTask extends UrlJsonAsyncTask {
+	    public PlayerTask(Context context) {
+	        super(context);
+	    }
+
+	    @Override
+	    protected JSONObject doInBackground(String... urls) {
+	    	StringBuilder builder = new StringBuilder();
+		    HttpClient client = new DefaultHttpClient();
+		    HttpGet httpGet = new HttpGet("http://cryptic-hollows-1268.herokuapp.com/users.json");
+		    
+	        HttpResponse response1 = null;
+	        JSONObject json = new JSONObject();
+
+	        try {
+	            try {
+	                // setup the returned values in case
+	                // something goes wrong
+	                
+	                // add the user email and password to
+	                // the params
+	                response1 = client.execute(httpGet);
+	                StatusLine statusLine = response1.getStatusLine();
+	    		    int statusCode = statusLine.getStatusCode();
+	    		    if(statusCode == 200)
+	    		    {
+	                HttpEntity entity = response1.getEntity();
+	    	        InputStream content = entity.getContent();
+	    	        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+	    	        String line;
+	    	        while ((line = reader.readLine()) != null) {
+	    	          builder.append(line);
+	    	        }
+	    	        JSONArray arr = new JSONArray(builder.toString());
+	                json = json.put("a", arr);
+	    		    }
+
+	            } catch (HttpResponseException e) {
+	                e.printStackTrace();
+	                Log.e("ClientProtocol", "" + e);
+	                json.put("info", "Unable to Fetch Players");
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	                Log.e("IO", "" + e);
+	            }
+	        } catch (JSONException e) {
+	            e.printStackTrace();
+	            Log.e("JSON", "" + e);
+	        }
+
+	        return json;
+	    }
+
+	    @Override
+	    protected void onPostExecute(JSONObject json) {
+	        try {
+	        	Log.i(ViewPlayers.class.getName(), json.toString());
+	        	 JSONArray jsonArray = json.getJSONArray("a");
+	        	 String[] players =new String[jsonArray.length()]; 
+			      Log.i(ViewPlayers.class.getName(),
+			          "Number of entries " + jsonArray.length());
+			      for (int i = 0; i < jsonArray.length(); i++) {
+			        JSONObject jsonObject = jsonArray.getJSONObject(i);
+			        players[i]= jsonObject.getString("name");
+			        Log.i(ViewPlayers.class.getName(), jsonObject.getString("name"));
+			      }
+			      
+			      setListAdapter(new PlayerArrayAdapter(context, players));
+		            
+		            Toast.makeText(context, json.getString("info"), Toast.LENGTH_LONG).show();
+			    } catch (Exception e) {
+			      e.printStackTrace();
+			    }	
+	        finally {
+	            super.onPostExecute(json);
+	        }
+	    }
+	}
+	
 }
 
